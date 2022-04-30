7f9f83aa580265ad2d364a2c8363d8136c475305
Dmitry Marov <d.marov94@gmail.com>
2022-04-30 12:15:56 +0300

    updated instruction

--- install-windows.ps1
+++ install-windows.ps1
@@ -1,3 +1,4 @@
+# from admin
 choco install git -y
 choco install python3 -y
 choco install python2 -y
@@ -12,20 +13,20 @@ choco install neovim -y
 choco install cmake -y
 choco install golang -y
 choco install ojdkbuild -y
-choco install nvm -y
 choco install ruby2.devkit -y
 choco install gem -y
 gem install neovim
 choco install pip -y --allow-empty-checksums
 choco install pip2 -y --allow-empty-checksums
 
-#pip2.exe install neovim
+# pip2.exe install neovim
 pip3 install neovim
 pip3 install --user pynvim
 npm install -g neovim typescript-language-server
 
 Set-ExecutionPolicy unrestricted
 
+# from user
 # create folders
 New-Item -ItemType Directory -Path "~/AppData/Local/nvim/autoload" -Force
 New-Item -ItemType Directory -Path "~/AppData/Local/nvim/plugged" -Force
@@ -34,6 +35,7 @@ New-Item -ItemType Directory -Path "~/AppData/Local/nvim/colors" -Force
 # install plug
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -OutFile "$home/AppData/Local/nvim/autoload/plug.vim"
 
+# from admin
 # install config
 New-Item -ItemType SymbolicLink -Path "$home/AppData/Local/nvim/init.vim" -Target "$pwd/init-win.vim" -Force
 New-Item -ItemType SymbolicLink -Path "$home/AppData/Local/nvim/ginit.vim" -Target "$pwd/ginit.vim" -Force
