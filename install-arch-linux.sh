8f4f5c90e335218c616e9af336f16d8a11f5a0f6
Dmitry Marov <d.marov94@gmail.com>
2023-02-09 23:50:59 +0300

    removed old config

--- install-arch-linux.sh
+++ install-arch-linux.sh
@@ -4,32 +4,17 @@
 
 # requirements
 sudo pacman -S --needed \
-    curl \
-    git \
-    npm \
-#    node \
     xclip \
     neovim \
-#    coreutils \
     python3 \
     python-pip \
     python2-pip \
-#    py35-setuptools \
-#    libexo \
-#    liberation-fonts-ttf \
-#    global \
     ctags \
-    powerline \
-    poverline-fonts \
-    poverline-vim \
-    ttf-hack \
-#    rubygem-gems \
-#    the_silver_searcher \
+    ttf-hack
 
 sudo pip install neovim
 sudo pip install neovim-remote
 
-# install config
 ln -sf `pwd` ~/.config/nvim
 
 cd ~
